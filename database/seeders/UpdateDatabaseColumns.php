5b1727689000a9944c65b61733ca057e733facde
taimoor5356 <mtaimoornaveed@gmail.com>
2023-11-15 00:55:06 +0500

    added command to add $ sign

---
+++ database/seeders/UpdateDatabaseColumns.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UpdateDatabaseColumns extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Add currency
+        DB::table('items')->where('currency', '')->update([
+            'currency' => '$'
+        ]);
+    }
+}
