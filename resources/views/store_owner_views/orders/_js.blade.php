6c214eabb3d9840f7dd28897c42e035e775b6609
taimoor5356 <mtaimoornaveed@gmail.com>
2023-12-13 20:31:39 +0500

    working on store owner dashboard

--- resources/views/store_owner_views/orders/_js.blade.php
+++ resources/views/store_owner_views/orders/_js.blade.php
@@ -1,5 +1,13 @@
 <script>
     $(document).ready(function() {
-        $('.datatable').DataTable();
+        $('.datatable').DataTable({
+            ajax: {
+                url: "{{route('get_vendor_orders')}}",
+                data: {
+                    _token: "{{csrf_token()}}",
+                    vendor_id: 2
+                },
+            }
+        });
     });
 </script>
