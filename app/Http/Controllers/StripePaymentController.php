60c1ebe296cfdd5c90e42a414d304882d5c3435b
taimoor5356 <mtaimoornaveed@gmail.com>
2023-12-27 20:06:02 +0500

    updated success url in stripe

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -64,7 +64,7 @@ class StripePaymentController extends Controller
                 'quantity' => 1,
             ]],
             'mode' => 'payment',
-            'success_url' => $YOUR_DOMAIN . 'pay-stripe/success?order_id='.$order->id.'&transaction_reference='.$tran,
+            'success_url' => $YOUR_DOMAIN . 'pay-stripe/success?success=true&order_id='.$order->id.'&transaction_reference='.$tran,
             'cancel_url' => url()->previous(),
         ]);
 
